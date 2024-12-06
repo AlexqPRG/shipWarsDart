61479a64c0300fb8227d5645e49df2ba95f47252
Myts A.V <myts.shura@mail.ru>
2024-12-06 15:00:42 +0300

    UPDATIND

--- bin/water_fight.dart
+++ bin/water_fight.dart
@@ -1,10 +1,26 @@
 import 'dart:io';
+import 'dart:isolate';
 import 'dart:math';
+import 'dart:async';
 
 import 'package:water_fight/water_fight.dart' as water_fight;
 
 Random random = Random();
 bool botMode = false;
+
+bool failAttack = false;
+
+final String logPath = "watShipLog.txt";
+final String statPath = "watShipStat.txt";
+
+void log(SendPort sendPort){
+  final receivePort = ReceivePort();
+  sendPort.send(receivePort.sendPort);
+
+  receivePort.listen((message){
+    File(logPath).writeAsStringSync("$message\n", mode: FileMode.append);
+  });
+}
 
 void clean(){
   stdout.write('\x1B[2J\x1B[0;0H');
@@ -31,6 +47,8 @@ class Player{
   List<Ship> ships;
   int count = 0;
   int successAttack = 0;
+  int missedAttack = 0;
+  List<String> previousAttck = [];
 
   Player(this.name, int size, this.ships){
     pole = List.generate(size, (_) => List.filled(size, "[ ]"));
@@ -120,6 +138,15 @@ class Player{
       }
     }
   }
+  
+  void statistika(){
+    File(statPath).writeAsStringSync("Статистика игрока: $name:\nКоличество попаданий: $successAttack\nКоличество промахов: $missedAttack\n\n", mode: FileMode.append);
+  }
+
+  bool isPrevious(int x, int y){
+    return previousAttck.contains("$x:$y");
+  }
+
 }
 
 class Bot extends Player{
@@ -153,19 +180,27 @@ class Bot extends Player{
 }
 
 String attack(int x, int y, Player player1, Player player2){
+  if(player2.isPrevious(x, y)){
+    return "Ошибка, вы уже ходили на это поле";
+  }
   if(player1.pole[x][y] != "[ ]" && player1.pole[x][y] != "[X]" && player1.pole[x][y] != "[0]"){
     player2.gamePole[x][y] = "[X]";
     player1.pole[x][y] = "[X]";
     player2.successAttack++;
+    player2.previousAttck.add("$x:$y");
     player2.checkWin();
+    player2.statistika();
     return "Попал";
   }
   player2.gamePole[x][y] = "[0]";
   player1.pole[x][y] = "[0]";
+  player2.missedAttack++;
+  player2.previousAttck.add("$x:$y");
+  player2.statistika();
   return "Промах";
 }
 
-void game(Player player1, Player player2){
+void game(Player player1, Player player2, SendPort sendPort) async{
   bool firstPlayer = true;
 
   while(!player1.win && !player2.win){
@@ -173,6 +208,8 @@ void game(Player player1, Player player2){
     String result;
       do{
           firstPlayer ? print(player1.printPole()) : print(player2.printPole());
+          failAttack = false;
+
           if(firstPlayer){
             print("Введите координату X:");
             String? xInput = stdin.readLineSync();
@@ -182,6 +219,11 @@ void game(Player player1, Player player2){
             String? yInput = stdin.readLineSync();
             var yInputValue = int.tryParse(yInput!);
             result = attack(xInputValue!-1, yInputValue!-1, firstPlayer ? player2 : player1, firstPlayer ? player1 : player2);
+            // player1.log("Игрок: ${player1.name} сделал ход на: $xInputValue $yInputValue и итог: $result");
+            sendPort.send("Игрок: ${player1.name} сделал ход на: $xInputValue $yInputValue и итог: $result");
+            if(result == "Ошибка, вы уже ходили на это поле"){
+              failAttack = true;
+            }
             print(result);
           }else{
             if(player2 is Bot){
@@ -196,21 +238,69 @@ void game(Player player1, Player player2){
               String? yInput = stdin.readLineSync();
               var yInputValue = int.tryParse(yInput!);
               result = attack(xInputValue!-1, yInputValue!-1, firstPlayer ? player2 : player1, firstPlayer ? player1 : player2);
+              sendPort.send("Игрок: ${player2.name} сделал ход на: $xInputValue $yInputValue и итог: $result");
+              // player2.log("Игрок: ${player2.name} сделал ход на: $xInputValue $yInputValue и итог: $result");
+              if(result == "Ошибка, вы уже ходили на это поле"){
+                failAttack = true;
+              }
               print(result);
             }
           }
-      }while(result == "Попал" && !player1.win && !player2.win);
+      }while(result == "Попал" && !player1.win && !player2.win || failAttack);
 
       firstPlayer = !firstPlayer;
       clean();
   }
   clean();
   print("Выиграл игрок: ${player1.win ? player1.name : player2.name}");
+
+  File(logPath).writeAsStringSync('');
+  File(statPath).writeAsStringSync('');
+  workWithFile(player1);
+  workWithFile(player2);
 }
 
 
+void workWithFile(Player player){
+  int gamesPLayed = 0;
+  int countOfWins = 0;
+  int countOfLose = 0;
+  if(File("${player.name}.txt").existsSync()){
+    String text = File("${player.name}.txt").readAsStringSync();
+    List<String> text_list = text.split("\n");
 
-void main(List<String> arguments) {
+    
+
+    for(var item in text_list){
+      if(item.startsWith("Сыграно игр:")){
+        gamesPLayed = int.parse(item.replaceFirst("Сыграно игр:", "").trim());
+      }else if(item.startsWith("Количество побед:")){
+        countOfWins = int.parse(item.replaceFirst("Количество побед:", "").trim());
+      }else if(item.startsWith("Количество поражений")){
+        countOfLose = int.parse(item.replaceFirst("Количество поражений:", "").trim());
+      }
+    }
+    gamesPLayed++;
+    player.win ? countOfWins++ : countOfLose++;
+    File("${player.name}.txt").writeAsStringSync("Сыграно игр:$gamesPLayed\nКоличество побед:$countOfWins\nКоличество поражений:$countOfLose");
+  }else{
+    player.win ? countOfWins++ : countOfLose++;
+    gamesPLayed = 1;
+    File("${player.name}.txt").writeAsStringSync("Сыграно игр:$gamesPLayed\nКоличество побед:$countOfWins\nКоличество поражений:$countOfLose");
+
+  }
+}
+
+Future<void> zadershka() async{
+  await Future.delayed(Duration(seconds: 2));
+}
+
+void main(List<String> arguments)async{
+  final receivePort = ReceivePort();
+  await Isolate.spawn(log, receivePort.sendPort);
+
+  final sendPort = await receivePort.first as SendPort;
+  
   while(true){
     print("Выберите режим игры:\n1 - Игра друг с другом\n2 - Игра против бота");
     String? inputMode = stdin.readLineSync();
@@ -252,7 +342,7 @@ void main(List<String> arguments) {
     String? nameFirst = stdin.readLineSync();
     Player player1 = Player(nameFirst!, size, ships);
     player1.placingShips();
-
+    await zadershka();
     clean();
 
     Bot bot = Bot("Машина", size, ships);
@@ -267,6 +357,6 @@ void main(List<String> arguments) {
       clean();
     }
     
-    game(player1, botMode ? bot : player2);
+    game(player1, botMode ? bot : player2, sendPort);
   }
 }
